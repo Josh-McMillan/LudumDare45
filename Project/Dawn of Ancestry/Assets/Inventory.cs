707c39432cf0dce73eeb0d6bc26ef40c0aa8187c
Joshua McMillan <jmcmillan.programming@gmail.com>
2019-10-05 00:18:56 -0500

    Added Resource Gathering
    Resource Gathering is currently NON-tool specific.
    Added Food, Wood, and Stone Resources.

---
+++ Project/Dawn of Ancestry/Assets/Inventory.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType
+{
+    FOOD,
+    WOOD,
+    STONE
+}
+
+public class Inventory : MonoBehaviour
+{
+    public static Action<string> OnFoodUpdate;
+    public static Action<string> OnWoodUpdate;
+    public static Action<string> OnStoneUpdate;
+
+    private static int food = 0;
+    private static int wood = 0;
+    private static int stone = 0;
+
+    public static void GatherResource(ResourceType type, int amount)
+    {
+        switch (type)
+        {
+            case ResourceType.FOOD:
+                food += amount;
+                OnFoodUpdate(food.ToString());
+                break;
+
+            case ResourceType.WOOD:
+                wood += amount;
+                OnWoodUpdate(wood.ToString());
+                break;
+
+            case ResourceType.STONE:
+                stone += amount;
+                OnStoneUpdate(stone.ToString());
+                break;
+        }
+
+        Debug.Log("INVENTORY: " + food + " FOOD | " + wood + " WOOD | " + stone + " STONE");
+    }
+
+    public static bool CheckResourceAvailable(ResourceType type, int amount)
+    {
+        switch (type)
+        {
+            case ResourceType.FOOD:
+                if (food >= amount)
+                {
+                    return true;
+                }
+                break;
+
+            case ResourceType.WOOD:
+                if (wood >= amount)
+                {
+                    return true;
+                }
+                break;
+
+            case ResourceType.STONE:
+                if (stone >= amount)
+                {
+                    return true;
+                }
+                break;
+        }
+
+        return false;
+    }
+
+    public static void ConsumeResource(ResourceType type, int amount)
+    {
+        switch (type)
+        {
+            case ResourceType.FOOD:
+                food -= amount;
+
+                if (food < 0)
+                {
+                    food = 0;
+                }
+
+                OnFoodUpdate(food.ToString());
+
+                break;
+
+            case ResourceType.WOOD:
+                stone -= amount;
+
+                if (stone < 0)
+                {
+                    stone = 0;
+                }
+
+                OnWoodUpdate(wood.ToString());
+
+                break;
+
+            case ResourceType.STONE:
+                stone -= amount;
+
+                if (stone < 0)
+                {
+                    stone = 0;
+                }
+
+                OnStoneUpdate(stone.ToString());
+
+                break;
+        }
+    }
+}
---
+++ Project/Dawn of Ancestry/Assets/Resource.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    [SerializeField] private ResourceType resource;
+
+    [SerializeField] private float collectionTime = 2.0f;
+
+    private bool playerNear = false;
+
+    private bool canCollect = true;
+
+    private WaitForSeconds waitTime;
+
+    private void Start()
+    {
+        waitTime = new WaitForSeconds(collectionTime);
+    }
+
+    private void Update()
+    {
+        if (playerNear)
+        {
+            if (Input.GetMouseButton(0) && canCollect)
+            {
+                Debug.Log("Player is collecting resource!");
+                StartCoroutine(CollectResource());
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Player is near resource!");
+        playerNear = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Player has left resource!");
+        playerNear = false;
+    }
+
+    IEnumerator CollectResource()
+    {
+        canCollect = false;
+        Inventory.GatherResource(resource, 1);
+
+        yield return waitTime;
+
+        canCollect = true;
+    }
+}
